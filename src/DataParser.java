8e5c04a4ab47fe287cee319de818b6d1433adf7c
rhit-grunindm <dvdgruninger@gmail.com>
2021-03-25 10:44:21 -0400

    Refactored PlayerService, Main, and DatabaseConnectionSerive (with Alan)

--- src/DataParser.java
+++ src/DataParser.java
@@ -10,39 +10,48 @@ import java.util.StringTokenizer;
 import sodabase.services.DatabaseConnectionService;
 
 public class DataParser {
-
+	// Rename and get rid of String[] args
 	public static void main(String[] args, DatabaseConnectionService dbService) {
 		String fileName = "DataSheet.csv";
 		File file = new File(fileName);
-		CallableStatement callableStatement = null;
 		try {
 			Scanner inputStream = new Scanner(file);
 			inputStream.nextLine();
 			while (inputStream.hasNext()) {
-				String data = inputStream.nextLine();
-				String[] values = data.split(",");
-				try {
-					callableStatement = dbService.getConnection()
-							.prepareCall("{?=call insertData(?,?,?,?,?,?,?,?,?,?)}");
-					callableStatement.registerOutParameter(1, Types.INTEGER);
-					callableStatement.setString(2, values[0]);
-					callableStatement.setString(3, values[1]);
-					callableStatement.setString(4, values[2]);
-					callableStatement.setString(5, values[3]);
-					callableStatement.setString(6, values[4]);
-					callableStatement.setString(7, values[5]);
-					callableStatement.setString(8, values[6]);
-					callableStatement.setString(9, values[7]);
-					callableStatement.setString(10, values[8]);
-					callableStatement.setString(11, values[9]);
-					callableStatement.execute();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+				insertData(dbService, inputStream);
 			}
 			inputStream.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void insertData(DatabaseConnectionService dbService, Scanner inputStream) {
+		String data = inputStream.nextLine();
+		String[] values = data.split(",");
+		try {
+			executeInsertDataStatement(dbService, values);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void executeInsertDataStatement(DatabaseConnectionService dbService, String[] values)
+			throws SQLException {
+		CallableStatement callableStatement;
+		callableStatement = dbService.getConnection()
+				.prepareCall("{?=call insertData(?,?,?,?,?,?,?,?,?,?)}");
+		callableStatement.registerOutParameter(1, Types.INTEGER);
+		callableStatement.setString(2, values[0]);
+		callableStatement.setString(3, values[1]);
+		callableStatement.setString(4, values[2]);
+		callableStatement.setString(5, values[3]);
+		callableStatement.setString(6, values[4]);
+		callableStatement.setString(7, values[5]);
+		callableStatement.setString(8, values[6]);
+		callableStatement.setString(9, values[7]);
+		callableStatement.setString(10, values[8]);
+		callableStatement.setString(11, values[9]);
+		callableStatement.execute();
 	}
 }
--- src/sodabase/services/DatabaseConnectionService.java
+++ src/sodabase/services/DatabaseConnectionService.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 
 public class DatabaseConnectionService {
 
-	//DO NOT EDIT THIS STRING, YOU WILL RECEIVE NO CREDIT FOR THIS TASK IF THIS STRING IS EDITED
 	private final String SampleURL = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}}";
 
 	private Connection connection = null;
@@ -15,27 +14,27 @@ public class DatabaseConnectionService {
 	private String serverName;
 
 	public DatabaseConnectionService(String serverName, String databaseName) {
-		//DO NOT CHANGE THIS METHOD
 		this.serverName = serverName;
 		this.databaseName = databaseName;
 	}
 
 	public boolean connect(String user, String pass) {
-		//TODO: Task 1
-		//BUILD YOUR CONNECTION STRING HERE USING THE SAMPLE URL ABOVE
-		String replaceString = SampleURL.replace("${dbServer}", serverName);
-		String replaceString2 = replaceString.replace("${dbName}", databaseName);
-		String replaceString3 = replaceString2.replace("${user}", user);
-		String connectionString = replaceString3.replace("${pass}", pass);
-//		String connectionString = "jdbc:sqlserver://" + serverName + ";databaseName=" + databaseName + ";user=" + user + ";password={" + pass + "}";
+		String connectionString = prepareSQLString(user, pass);
 		try {
 			this.connection = DriverManager.getConnection(connectionString);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
 		return true;
+	}
+
+	private String prepareSQLString(String user, String pass) {
+		String replaceString = SampleURL.replace("${dbServer}", serverName);
+		replaceString = replaceString.replace("${dbName}", databaseName);
+		replaceString = replaceString.replace("${user}", user);
+		replaceString = replaceString.replace("${pass}", pass);
+		return replaceString;
 	}
 	
 
@@ -44,11 +43,9 @@ public class DatabaseConnectionService {
 	}
 	
 	public void closeConnection() {
-		//TODO: Task 1
 		try {
 			this.connection.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
