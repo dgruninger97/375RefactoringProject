50760b71497422bff4c70686d699493f47b45c87
rhit-grunindm <dvdgruninger@gmail.com>
2021-03-28 18:59:25 -0400

    Fixed weird bug with no search results

--- src/sodabase/services/TeamWindow.java
+++ src/sodabase/services/TeamWindow.java
@@ -114,6 +114,7 @@ public class TeamWindow {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				searchButton(rdbtnGame, rdbtnSeason, rdbtnFranchise, formattedTextField);
+				buttonSelection = -1;
 			}
 
 		});
