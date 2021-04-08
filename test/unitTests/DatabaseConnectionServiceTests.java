e7f4ca720b7ff5bbc45c9a4817213c31d3f64822
Aden Khan <adenkhan4000@gmail.com>
2021-04-08 10:28:56 -0400

    DatabaseConnectionServiceTests

--- test/unitTests/DatabaseConnectionServiceTests.java
+++ test/unitTests/DatabaseConnectionServiceTests.java
@@ -1,14 +1,39 @@
 package unitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import services.DatabaseConnectionService;
 
 class DatabaseConnectionServiceTests {
 
+	static DatabaseConnectionService instance;
+	
+	static String serverName = "nba-database.csse.rose-hulman.edu";
+	static String databaseName = "TestDatabase";
+	static String user = "sa";
+	static String pass = "Thesquarerootoftheradiusofthesun123";
+	
+	String expectedURL = "jdbc:sqlserver://nba-database.csse.rose-hulman.edu:1433;";
+			
+	@BeforeAll
+	static void Setup() {
+		instance = new DatabaseConnectionService(serverName, databaseName);
+		instance.connect(user, pass);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TestConnection() {
+		Connection connection = instance.getConnection();
+		
+		try {
+			assertTrue(connection.getMetaData().getURL().startsWith(expectedURL));
+		} catch (SQLException e) {
+			fail("SQLException :: " + e.getMessage());
+		}
 	}
 
 }
