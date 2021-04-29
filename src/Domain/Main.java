199a6cc865ed2e01d47ecf8d720374dca747bd33
rhit-grunindm <dvdgruninger@gmail.com>
2021-04-29 11:43:29 -0400

    Adding in config options for user

--- src/Domain/Main.java
+++ src/Domain/Main.java
@@ -1,18 +1,12 @@
 package Domain;
-import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.border.Border;
-
-import DataParsing.DataParser;
 import presentationWindows.NBADatabaseWindow;
-import sodabase.ui.ApplicationRunner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		DatabaseConnectionService dbService = new DatabaseConnectionService("nba-database.csse.rose-hulman.edu", "TestDatabase");
+		DatabaseConnectionService dbService = new DatabaseConnectionService("nba-database.csse.rose-hulman.edu",
+				"TestDatabase");
 		dbService.connect("sa", "Thesquarerootoftheradiusofthesun123");
 		NBADatabaseWindow mainWin = new NBADatabaseWindow();
 		mainWin.startMainWindow(dbService);
