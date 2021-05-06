d121decc6fc8200cfb52e5025f72a888ea8f99b3
rhit-grunindm <dvdgruninger@gmail.com>
2021-05-06 10:25:38 -0400

    Moved server login info to text files

--- src/Domain/Main.java
+++ src/Domain/Main.java
@@ -1,13 +1,37 @@
 package Domain;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import presentationWindows.NBADatabaseWindow;
 
 public class Main {
 
 	public static void main(String[] args) {
-		DatabaseConnectionService dbService = new DatabaseConnectionService("nba-database.csse.rose-hulman.edu",
+		String serverName = "";
+		String username = "";
+		String password = "";
+	    try {
+	        File serverFile = new File("server.txt");
+	        Scanner myReader = new Scanner(serverFile);
+            serverName =  myReader.nextLine();
+            myReader.close();
+            File userFile = new File("username.txt");
+            myReader = new Scanner(userFile);
+            username = myReader.nextLine();
+            myReader.close();
+            File passFile = new File("password.txt");
+            myReader = new Scanner(passFile);
+            password = myReader.nextLine();
+            myReader.close();
+	      } catch (FileNotFoundException e) {
+	        System.out.println("An error occurred.");
+	        e.printStackTrace();
+	    }
+		DatabaseConnectionService dbService = new DatabaseConnectionService(serverName,
 				"TestDatabase");
-		dbService.connect("sa", "Thesquarerootoftheradiusofthesun123");
+		dbService.connect(username, password);
 		NBADatabaseWindow mainWin = new NBADatabaseWindow();
 		mainWin.startMainWindow(dbService);
 	}
