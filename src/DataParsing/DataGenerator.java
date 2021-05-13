fa814790e66da17d875f338fcdf2b92ef8cd71c8
Aden Khan <adenkhan4000@gmail.com>
2021-05-13 16:13:52 -0400

    Moving and updating tests

--- src/DataParsing/DataGenerator.java
+++ src/DataParsing/DataGenerator.java
@@ -14,7 +14,7 @@ public class DataGenerator {
 			"Portland Trail Blazers", "Utah Jazz"};
 	
 	public static String[] yearList = {"2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009",
-			"2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"};
+			"2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"};
 	
 	public static void main(String[] args) throws IOException {
 		loadHashMap();
@@ -83,7 +83,7 @@ public class DataGenerator {
 		csvWriter.close();
 	}
 
-	private static void loadHashMap() {
+	public static void loadHashMap() {
 		playerTeamMap.put("Kobe Bryant", "Los Angeles Lakers");
 		playerTeamMap.put("Paul George", "Indiana Pacers");
 		playerTeamMap.put("Steph Curry", "Golden State Warriors");
